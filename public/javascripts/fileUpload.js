731bf8a3d4b52283650290b3aff24453b8fd35e2
3rda <kemaltirta@gmail.com>
2023-02-15 20:29:18 +0700

    Books Create/Index/New 3

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,11 @@
+// Using FilePond Script
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+  )
+
+FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100
+})
+FilePond.parse(document.body);
